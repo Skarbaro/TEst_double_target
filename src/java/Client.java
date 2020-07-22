912197d3957d7b526b68ccd37a79feb748c96a54
Alaster <alaster.raf@gmail.com>
2020-07-22 10:10:50 +0300

    add sys

---
+++ src/java/Client.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Client {
+
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 15;
+		int sum;
+		
+		sum = num1 / num2;
+		System.out.println("Sum of these numbers: " + sum);
+	}
+}
