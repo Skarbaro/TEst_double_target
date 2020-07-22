0f82e6caad142ce6bee0fe34c3df14b948cf7655
Alex Romaniuk <alaster.rom@gmail.com>
2020-07-22 13:28:34 +0300

    as

--- SimpleConsole.java
+++ SimpleConsole.java
@@ -1,22 +1,21 @@
-package src.tr0;
+package src;
 
 public class Example {
 	// вызов метода main().
 	public static void main(String args[]) {
-		int reg;
 		
 		logger.log("Простая программа на Java.");
 	}
 	
 	public static int ung(int var1, int var2) {
         int tempRezult = var1 * var2;
-        System.out.println("Rezult = " + tempRezult);
+        logger.log("Rezult = " + tempRezult);
         return tempRezult;
     }
 	
 	public static int min(int var1, int var2) {
         int tempRezult = var1 - var2;
-        System.out.println("Rezult = " + tempRezult);
+        logger.log("Rezult = " + tempRezult);
         return tempRezult;
     }
 	
