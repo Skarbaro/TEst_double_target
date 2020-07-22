8d386b6182625b8a2db81b74cb3506f147fe2310
Alex Romaniuk <alaster.rom@gmail.com>
2020-07-22 14:09:05 +0300

    add int string

--- DelNum.java
+++ DelNum.java
@@ -3,7 +3,8 @@ package src;
 public class DelNum {
 
    public static void main(String[] args) {
-      int num1 = 5, num2 = 15; 
+      int num1 = 5;
+	  int num2 = 15; 
 	  int dif;
       dif = num1 - num2;
       logger.log("Sum of these numbers: " + dif);
