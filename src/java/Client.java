e348387d068d1537a4d6d60f5d83594bbc7bab94
Alex Romaniuk <alaster.rom@gmail.com>
2020-07-23 11:32:09 +0300

    add 1 int

--- src/java/Client.java
+++ src/java/Client.java
@@ -5,7 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		int num1 = 5, num2 = 15;
 		int sum;
-		
+		int deletig;
 		sum = num1 / num2;
 		System.out.println("Sum of these numbers: " + sum);
 	}
