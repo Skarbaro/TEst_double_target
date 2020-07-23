de8a3c89cf0aef95534d401a74d5670fa2fb2f7f
Alaster <alaster.raf@gmail.com>
2020-07-23 13:04:35 +0300

    ass

--- src/TestApp.java
+++ src/TestApp.java
@@ -3,6 +3,7 @@ import infrastructure.config.utils.StringUtils;
 public class TestApp {
 	
 	private int reg_1;
+	private int reg_2;
 
     public static void main(String[] args) {
         StringUtils randomString = new StringUtils();
@@ -12,9 +13,10 @@ public class TestApp {
 	public static void main(String[] args) {
       int nu1 = 3, nu2 = 12, sun;
 	  int reg;
+	  private int reg_5;
 	  
       sun = nu1 + nu2;
-      System.out.println("Sun of these numbers: " + sun);
+      logger.log("Sun of these numbers: " + sun);
    }
 
 }
