1e5692c1ca63d12b638000b1fb626ea4ba1c0e03
Alaster <alaster.raf@gmail.com>
2020-07-24 14:11:23 +0300

    asd

---
+++ Variables.java
@@ -0,0 +1,10 @@
+package src;
+
+class Variables {
+	public static void main (String[] args) {
+		double a = 3, b = 4;
+		double c;
+		c = Math.sqrt (a* a + b* b);
+		System.out.println ("c = "+ c);
+	}
+}
