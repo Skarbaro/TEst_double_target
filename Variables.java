182ebeac1ae32ddf2e2e3d36ece932da4f2e996f
Alaster <alaster.raf@gmail.com>
2020-07-24 15:08:07 +0300

    asd

--- Variables.java
+++ Variables.java
@@ -1,9 +1,15 @@
 package src;
 
 class Variables {
+	
+	private int a2;
+	
 	public static void main (String[] args) {
 		double a = 3, b = 4;
 		double c;
+		
+		int ret4;
+		
 		c = Math.sqrt (a* a + b* b);
 		System.out.println ("c = "+ c);
 	}
