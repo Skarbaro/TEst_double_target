20fb7944d24d65a7c9a80e662724e066ed017f0b
Alaster <alaster.raf@gmail.com>
2020-07-29 09:07:01 +0300

    add

--- Variables.java
+++ Variables.java
@@ -1,17 +1,16 @@
-package src;
+package src.java;
 
 class Variables {
 	
-	private int a2;
+	private int a2, a1;
 	
 	public static void main (String[] args) {
 		double a = 3, b = 4;
 		double c;
 		
-		int ret4;
+		int ret4, ret5;;
 		
 		c = Math.sqrt (a* a + b* b);
-		System.out.println ("c = "+ c);
 		System.out.println ("c = "+ c);
 	}
 }
