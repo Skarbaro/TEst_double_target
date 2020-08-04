0567613122c0ebf59ced5e218a58fcf3497a574a
Alaster <alaster.raf@gmail.com>
2020-08-04 15:58:37 +0300

    chenge

--- src/TestApp.java
+++ src/TestApp.java
@@ -3,9 +3,11 @@ import infrastructure.config.utils.StringUtils;
 public class TestApp {
 	
 	private int reg_1;
-	private int reg_2;
 
     public static void main(String[] args) {
+		
+		private int reg_2;
+		
         StringUtils randomString = new StringUtils();
         System.out.println(randomString.randomString("alphanumeric", 50));
     }
@@ -13,10 +15,9 @@ public class TestApp {
 	public static void main(String[] args) {
       int nu1 = 3, nu2 = 12, sun;
 	  int reg;
-	  private int reg_5;
 	  
       sun = nu1 + nu2;
-      logger.log("Sun of these numbers: " + sun);
+      System.out.println("Sun of these numbers: " + sun);
    }
 
 }
