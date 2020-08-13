450d3c26a67b258538c0f5c9ba1f1702d36ae1b7
Alaster <alaster.raf@gmail.com>
2020-08-13 16:40:56 +0300

    fis issue 1

--- src/java/DifNumb.java
+++ src/java/DifNumb.java
@@ -8,7 +8,7 @@ public class DifNumbers {
       System.out.println("Sum of these numbers: " + dif);
    }
    
-   public static void main(String args[]) {
+   public static void main(String[] args) {
       int red1 = 5, red2 = 17, rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
