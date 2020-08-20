ca002b9027bac57b5fdceddafefa5d07bf784099
Alaster <alaster.raf@gmail.com>
2020-08-20 11:58:18 +0300

    move 1 / del 2

--- src/java/terra/Home.java
+++ src/java/terra/Home.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework1 {
+	
+	private int reno, reno0;;
 
     public static void main(String[] args) {
 		
 		int ret9, red5 = 6;;;
-		int ford, telled, see;
+		int ford, see;
 
         WebDriver browser = new ChromeDriver();
         browser.get("https://google.com");
