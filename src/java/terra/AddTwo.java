b71e86e0ee6cec60a57fa856a7ca408182814ced
Alaster <alaster.raf@gmail.com>
2020-08-20 13:02:23 +0300

    fix

--- src/java/terra/AddTwo.java
+++ src/java/terra/AddTwo.java
@@ -5,16 +5,16 @@ public class AddTwo {
    public static void main(String[] args) {
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
-      System.out.println("Sum of these numbers: "+sum);
+      Slogger.log("Sum of these numbers: "+sum);
    }
    public static void main(String[] args) {
       int nu1 = 7, nu2 = 15, su;
       su = nu1 / nu2;
-      System.out.println("Sum of these numbers: "+su);
+      logger.log("Sum of these numbers: "+su);
    }
    public static void main(String[] args) {
-      int num1 = 5, num2 = 15, sim;
-      sim = num1 * num2;
+      int sim = 7;
+
       System.out.println("Sum of these numbers: "+sim);
    }
 }
