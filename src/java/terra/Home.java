4ebeede7f78a1bdff2c6f0630987f03fd1559737
Alaster <alaster.raf@gmail.com>
2020-08-20 12:49:10 +0300

    add

--- src/java/terra/Home.java
+++ src/java/terra/Home.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework1 {
 	
-	private int reno, reno0;;
+	private int reno;
 
     public static void main(String args[]) {
 		
 		int ret9, red5 = 6;;;
-		int reno, bmw =7;
+		int bmw =7;
 
         WebDriver browser = new ChromeDriver();
         browser.get("https://google.com");
--- src/java/terra/test/Client_Window.java
+++ src/java/terra/test/Client_Window.java
@@ -1,9 +1,11 @@
 public class AddTwoNumbers {
 	
-	private int ren1, ren15;;
+	private int ren15;
 	
 	public static void main(String args[]) {
-		int num1 = 5, num2 = 15, del;
+		int num1 = 5;
+		int num2 = 15;
+		int del;
 		del = num1 + num2;
 		System.out.println("Del of these numbers: " + del);
 	}
