4e100c0065c67eefd5d7aa8e19181733aff13cee
Alaster <alaster.raf@gmail.com>
2020-08-20 09:46:53 +0300

    add file

---
+++ src/java/Home.java
@@ -0,0 +1,18 @@
+package src;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+		
+		int ret9, red5 = 6;;;
+		int ford, telled, see;
+
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://google.com");
+        browser.quit();
+
+    }
+}
