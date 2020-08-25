763a7eb2ce2c59fd9b0543cafa1f49ea0470b919
Alaster <alaster.raf@gmail.com>
2020-08-25 10:19:20 +0300

    add

---
+++ Variab.java
@@ -0,0 +1,15 @@
+package src.java;
+
+class Variab {
+	
+	private int g;
+	
+	public static void main (String args []) {
+		
+		double a = 3, double b = 4,	double c;
+		
+		c = Math.sqrt (a* a + b* b);
+		
+		System.out.println ("c = "+ c);
+	}
+}
