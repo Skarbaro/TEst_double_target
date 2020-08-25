5a5f0e62743e80dd62c86ebece9f254539ef4bff
Alaster <alaster.raf@gmail.com>
2020-08-25 15:05:43 +0300

    addd

---
+++ src/java/Variables.java
@@ -0,0 +1,28 @@
+package src.java.test;
+
+class Variables {
+	
+	 private int a2, a4;
+	
+	public static void main (String[] args) {
+		
+		double a = 3;
+		int b = 4, c;
+		
+		int ret5;;
+		
+		c = Math.sqrt (a* a + b* b);
+		System.out.println("c = "+ ret5);
+	}
+	
+	public static void main(String args[]) {
+		
+      int nm1 = 5, nm2 = 15;
+	  int sum;
+      sum = nm1 + nm2;
+      System.out.println("Sum of these numbers: " + sum);
+	  
+   }
+   
+    
+}
