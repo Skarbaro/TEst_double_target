3e29800d6f116780b74d9987be8474fa17e0d0eb
Alaster <alaster.raf@gmail.com>
2020-08-28 10:31:09 +0300

    del and add

---
+++ src/java/terra/DifNumbers.java
@@ -0,0 +1,11 @@
+public class DifNumbers {
+	
+	private int ad, cd;
+
+   public static void main(String args[]) {
+      int num1 = 5, num2 = 15; 
+	  int dif;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
+}
