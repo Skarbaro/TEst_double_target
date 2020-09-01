f688659dca2266a43b523a68014b98cea35d5c8e
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-01 11:50:15 +0300

    add

--- src/java/terra/DifNumbers.java
+++ src/java/terra/DifNumbers.java
@@ -1,12 +1,11 @@
 package src.java;
 
+private int ad, dc;
+
 public class DifNumbers {
-	
-	private int ad;
 
    public static void main(String[] args) {
-      int num1 = 5;
-	  int num2 = 15; 
+      int num1 = 5, num2 = 15; 
 	  int dif;
       dif = num1 - num2;
       logger.log("Sum of these numbers: " + dif);
