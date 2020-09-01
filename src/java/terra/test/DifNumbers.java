81fa8344674bcbddac5170aa698f1f794f286fa8
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-01 12:03:42 +0300

    as

---
+++ src/java/terra/test/DifNumbers.java
@@ -0,0 +1,12 @@
+package src;
+
+private int ad, dc;
+
+public class DifNumbers {
+
+   public static void main(String args[]) {
+      int num1 = 5, num2 = 15, dif;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
+}
