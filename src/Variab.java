63c64f794177b2bf3cc608cf36fa5119b89a280e
Alaster <alaster.raf@gmail.com>
2020-09-01 09:50:58 +0300

    fix

--- src/Variab.java
+++ src/Variab.java
@@ -2,14 +2,12 @@ package src.java;
 
 class Variab {
 	
-	private int abf, ref;
-	
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		
 		double a = 7, b = 9, c;
 		
 		c = Math.sqrt (a* a + b* b);
 		
-		System.out.println("c = "+ c);
+		logger.log("c = "+ c);
 	}
 }
