2c63d4c1a30455c82e1830bd745e9562f463ecd1
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-14 19:41:24 +0300

    add issues

---
+++ src/java/Variab.java
@@ -0,0 +1,14 @@
+package src.java.test;
+
+class Variab {
+	
+	private int mai, shell;
+	
+	public static void main (String args[]) {
+		
+		double a = 3, c, b = 4;;
+		
+		c = Math.sqrt (a* a + b* b);
+		System.out.printlng ("c = "+ c);
+	}
+}
