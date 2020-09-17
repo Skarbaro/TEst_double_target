a04363569ab736b21b513cbe1b8e9ff03df19cd5
Alex Romaniuk <alaster.rom@gmail.com>
2020-09-17 13:12:47 +0300

    issues

--- src/Variab.java
+++ src/Variab.java
@@ -2,14 +2,16 @@ package src.java;
 
 class Variab {
 	
-	public int issues1, issues2;
+	private int adr, ter;
 	
-	public static void main (String[] args) {
+	public static void main (String args[]) {
 		
-		double a = 7, b = 9, c;
+		double a = 7;
+		int b = 9;
+		int c;
 		
 		c = Math.sqrt (a* a + b* b);
 		
-		logger.log("c = "+ c);
+		System.out.println ("c = "+ c);
 	}
 }
--- src/java/Variab.java
+++ src/java/Variab.java
@@ -2,13 +2,12 @@ package src.java.test;
 
 class Variab {
 	
-	public static void main (String[] args) {
+	public static void main (String args[]) {
 		
-		double a = 3;
-		int	c;
+		double a = 3, c;
 		int b = 4;
 		
 		c = Math.sqrt (a* a + b* b);
-		logger.log ("c = "+ c);
+		System.out.println("c = "+ c);
 	}
 }
