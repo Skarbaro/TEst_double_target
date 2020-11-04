466e7700505233fb826a26e81e49cc0e5fe75340
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-04 12:37:34 +0200

    add fix and issues

--- src/java/Variab.java
+++ src/java/Variab.java
@@ -2,9 +2,10 @@ package src.java.test;
 
 class Variab {
 	
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		
-		double a = 3, c, b = 4;
+		double a = 3;
+		int c, b = 4;
 		
 		c = Math.sqrt (a* a + b* b);
 		System.out.println("c = "+ c);
--- src/java/terra/test/DifNumbers.java
+++ src/java/terra/test/DifNumbers.java
@@ -1,10 +1,13 @@
-package src;
+package src.java;
 
 public class DifNumbers {
+	
+	private int abs;
 
-   public static void main(String[] args) {
+   public static void main(String args[]) {
 	   
-      int num1 = 5, num2 = 15, dif;
+      int num1 = 5;
+	  int num2 = 15, dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
