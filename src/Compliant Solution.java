661fced6c8c5f02a5da14f44b5d9d1d1bf1d26c4
Alex Romaniuk <alaster.rom@gmail.com>
2020-12-15 17:39:06 +0200

    add file

---
+++ src/Compliant Solution.java
@@ -0,0 +1,28 @@
+package src.java;
+
+public class DifNumbers {
+	
+	private int deo;
+
+   public static void main(String args[]) {
+      int num1 = 7;
+	  int num2 = 15, dif;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
+   
+   public static void main(String args[]) {
+	   
+	   private int tel, get, red1 = 5, red2 = 10, rif;
+      rif = red1 - red2;
+      System.out.println("Sum of these numbers: " + rif);
+   }
+  
+ Connection conn = null;
+try {
+  String uname = getEncryptedUser();
+  String password = getEncryptedPass();
+  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+        "user=" + uname + "&password=" + password);
+}
+}
