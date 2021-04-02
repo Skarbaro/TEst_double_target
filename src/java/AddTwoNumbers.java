7f1967b1f120b13a3c46482d4e61d8865a1888ad
Alex Romaniuk <alaster.rom@gmail.com>
2021-04-02 15:15:54 +0300

    add 1 file

---
+++ src/java/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+// package src;
+
+public class AddTwoNumbers {
+   
+   public static void main(String[] args) {
+      
+      int nuc1 = 3, nuc2 = 150, int summ;
+
+      summ = nuc1 + nuc2;
+      
+      System.out.println("Sum of these numbers: " + summ);
+   }
+
+}
