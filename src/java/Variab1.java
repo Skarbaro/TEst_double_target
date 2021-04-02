9aea7300ed5fe62e0ccf91d54bd2ab253c5d2eb2
Alex Romaniuk <alaster.rom@gmail.com>
2021-04-02 09:32:11 +0300

    add file Varib. del 6 files, move 1 file

---
+++ src/java/Variab1.java
@@ -0,0 +1,15 @@
+package src.java;
+
+class Variab {
+	
+	public static void main (String[] args) {
+		
+		double a = 3;
+		int b = 4;
+		int c;
+		
+		c = Math.sqrt (a* a + b* b);
+		
+		System.out.println ("c = "+ c);
+	}
+}
